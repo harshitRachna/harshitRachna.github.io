a25c20ad23dda0786cbacea0017b2d46bbffe87c
harshit Dubey <dubey123harshit@outlook.com>
2020-06-05 21:55:51 +0530

    rodo is here

---
+++ robofriends/precache-manifest.e3f15637b4183cb0684e59be202e1544.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f644dea2eea253a322c0aecaccb4e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "149eed7557a1f2c5b8fb",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "24cb99962ba17d2a9ff3",
+    "url": "/static/css/main.9d05ca26.chunk.css"
+  },
+  {
+    "revision": "149eed7557a1f2c5b8fb",
+    "url": "/static/js/2.c4319b30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4319b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cb99962ba17d2a9ff3",
+    "url": "/static/js/main.535dd897.chunk.js"
+  },
+  {
+    "revision": "11dcf18877b9b57d9d7a",
+    "url": "/static/js/runtime-main.b6b728f3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
